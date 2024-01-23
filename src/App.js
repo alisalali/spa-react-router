06af2dfd78790a643d5a66d175026a9ad9e7fd4c
alisalali <alis.alali@hotmail.com>
2024-01-24 01:37:24 +0300

    Showing Error page with errorElement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 //jsx router browser
 // const routeDefinition = createRoutesFromElements(
@@ -23,15 +19,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/products",
-        element: <ProductsPage />,
-      },
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
     ],
   },
 ]);
