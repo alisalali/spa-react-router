640644e9581526cf8271eebd3d3fce154a2b9268
alisalali <alis.alali@hotmail.com>
2024-01-23 22:28:09 +0300

    Navigate between Pages using Link

--- src/App.js
+++ src/App.js
@@ -9,26 +9,25 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
 //jsx router browser
-const routeDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinition);
 
 // object in array of routes
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage />,
-//   },
-//   {
-//     path: "/products",
-//     element: <ProductsPage />,
-//   },
-// ]);
-
-const router = createBrowserRouter(routeDefinition);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
